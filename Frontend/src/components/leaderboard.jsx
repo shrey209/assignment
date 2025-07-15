5e71545dd7e4c8e3868ed2fe5951aeca5412c5c7
shrey209 <144334177+shrey209@users.noreply.github.com>
2025-07-15 21:26:58 +0530

    Update leaderboard.jsx

--- Frontend/src/components/leaderboard.jsx
+++ Frontend/src/components/leaderboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./Leaderboard.css";
+import "./leaderboard.css";
 
 const Leaderboard = () => {
   const [users, setUsers] = useState([]);
